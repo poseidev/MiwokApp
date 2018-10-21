bb1fbc1f2e34a1422301b308f0d6b388a150d7c1
hdnl <hdnd02@gmail.com>
2018-10-21 19:35:41 +0800

    1. activity_numbers renamed to word_list.xml. 2. word_list is set as the content of ColorsActivity, FamilyActivity, and PhrasesActivity.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,11 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,9 +11,9 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<Word>();
+        ArrayList<Word> words = new ArrayList<>();
 
         words.add(new Word("one", "lutti"));
         words.add(new Word("two", "otiiko"));
@@ -27,8 +23,8 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("six", "temmokka"));
         words.add(new Word("seven", "kenekaku"));
         words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "nine"));
-        words.add(new Word("ten", "ten"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "n'aacha"));
 
         WordAdapter itemsAdapter = new WordAdapter(this, words);
 
